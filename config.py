62a467f5ac57b60eafbbebb6c9fb98a8cad88e10
Mdaadm <71222643+Mdaadm@users.noreply.github.com>
2021-05-15 17:14:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Facebook token
+fb_access_token = 'token'
+
+# Facebook verification token
+fb_verify_token = 'token'
+
+# Vkontakte token
+token_vk = 'token'
+
+# Telegram bot token
+tg_token = 'token'
+
+# Credentials file for google sheets api
+cred_final = 'path'
+
+# Facebook group id
+fb_group_id = 'id'
+
+# base.csv file path (for payment recording)
+base_file = 'path'
+
+# Path to folder for saved files
+files_path = 'path'
+
+# Path to folder for saved payment screenshots
+pay_imgs_path = 'path'
+
+# Telegram support chat id
+group_id = 1
+
+# Id of pinned message in TG support group
+pinned_msg = 1
+
+# TG bot id
+bot_id = 1
+
+# Token for telegram payment (Юкасса)
+pay_token = 'token'
+
+# Free OCR api token (https://ocr.space/)
+ocr_token = 'token'
+
+# Path to database file
+db_file = 'path'
+
+# Developer id
+my_id = 1
+
+# Owner id
+owner_id = 1
+
+# TG bot name
+bot_name = '@name'
---
+++ messages.py
@@ -0,0 +1,62 @@
+from config import bot_name
+
+yuan_text = "text"
+
+yuan_text_vk = "text"
+
+rub_text = "text"
+
+rub_text_vk = "text"
+
+trial_text = "text"
+
+trial_text_vk = "text"
+
+send_email = "text"
+
+buttons_menu = "text"
+
+pay = "text"
+
+pay_vk = "text"
+
+pay_type = "text"
+
+close = "text"
+
+push_buttons = "text"
+
+non_working = "text"
+
+sheets_empty = "text"
+
+first_install = "text"
+
+turk = "text"
+
+coop = "text"
+
+shop = "text"
+
+support = "text"
+
+support_vk = "text"
+
+type_support = "text"
+
+get_better = "text"
+
+email_already_used = "text"
+
+edit_wrong_mail = "text"
+edit_true_mail = "text"
+edit_confirm = "text"
+
+mailing_tariffs = "text"
+mailing_message = "text"
+
+left3days = "text"
+left1day = "text"
+left_today = "text"
+
+info = "text"
