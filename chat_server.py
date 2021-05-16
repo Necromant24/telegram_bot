714529c61441fd02b4d76d3b8527953a898b2a30
Necromant24 <kesha.tkachenko2017@mail.ru>
2021-05-16 18:39:18 +0300

    semi working chat beta 1.0

---
+++ chat_server.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, request, send_from_directory, redirect
+import data_structs as ds
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello():
+    return redirect("/static/chat_app/chat5.html", code=302)
+
+
+
+@app.route('/chat', methods = ['POST'])
+def chat_message():
+    command = request.json['command']
+    answer = ''
+
+    if(command in ds.all_commands):
+        answer = ds.command_answers[command]
+    else:
+        answer = { "answer": "unknown command", "commands":[]}
+
+    return answer
+
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
+
+
+
+@app.route('/commands')
+def all_commands():
+    answer = { 'data': list(ds.all_commands) }
+    return  answer
