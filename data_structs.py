be4f97d898bc2c074d434ed38e3414ff2a3ce305
Necromant24 <kesha.tkachenko2017@mail.ru>
2021-05-18 23:33:44 +0300

    add test ws chat proto

--- data_structs.py
+++ data_structs.py
@@ -16,7 +16,7 @@ command_answers = \
         "/Оплата": { "answer": messages.pay_type, "commands": [("В рублях ₽ или в гривнах ₴",'/rub'), ("В юанях ¥", "/yuan"), ("Связаться с поддержкой",'/sup')]},
         "/Пробный период": { "answer": messages.trial_text, "commands":[("Оплата",'/pay'), ("Связаться с поддержкой",'/sup')]},
         "/Узнать больше": { "answer": "Узнайте как заблокировать рекламу, какие появились сервера и многое другое", "commands": [["Блог",'/url']]},
-        "/Для Туркменистана":  { "answer": messages.turk, "commands":[("Сайт обслуживания",'/url'), ("Как подключить?",'/url')]},
+        "/Для Туркменистана":  { "answer": messages.turk, "commands":[("Сайт обслуживания",'https://tm.zgc.su/'), ("Как подключить?",'https://sites.google.com/view/zgcvpn/try?authuser=0')]},
         "/Сотрудничество": { "answer": messages.coop, "commands":[["Сделать предложение",'/url']]},
         "/ZGC SHOP": { "answer": messages.shop, "commands":[("ZGC SHOP",'/url'), ("Связаться с поддержкой", '/market') ]},
         "/Связаться с поддержкой": { "answer": messages.coop, "commands":[("Первичная настройка", "/install"),("Другое", "/other"), ('ZGC SHOP','/market')]},
@@ -33,8 +33,6 @@ command_answers = \
         "/pay": {"answer": messages.first_install, "commands":[]},
         "/url": {"answer": messages.first_install, "commands":[]},
 
-
-
     }
 
 
@@ -45,3 +43,6 @@ callback_cmd_list = ['/urgent', '/install', '/other', '/market', '/rub', '/yuan'
 
 all_commands = command_answers.keys()
 print(all_commands)
+
+
+ws_admin_client_pairs = {'admin1': ['client1', 'client2']}
---
+++ ws_chat_server.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+
+
+unauthed_sockets = set()
+
+# support - support client
+support = set()
+
+support_client_dict = {}
+
+async def hello(websocket, path):
+    print(path)
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, "localhost", 5500)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
